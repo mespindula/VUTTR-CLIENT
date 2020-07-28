66a9846ce5ee41de1c89fc702760646554326e21
Maicon Machado de Espindula <maicon.espindula@poa01.local>
2020-07-27 21:12:23 -0300

    teste

--- urls.js
+++ urls.js
@@ -1,5 +1,5 @@
 module.exports = {
-    local: 'http://localhost:3001/',
+    local: 'http://localhost:3000/',
     prod: 'https://vuttr-test-prod.herokuapp.com/',
     qa: 'https://vuttr-qa.herokuapp.com/'
 }
